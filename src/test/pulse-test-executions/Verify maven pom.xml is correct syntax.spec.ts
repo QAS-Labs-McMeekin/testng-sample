50d44a8640d134752a6839162ff3196d0d87f951
kmcmeek91 <kylemcmeekin@qasymphony.com>
2017-02-16 11:37:58 -0500

    Test Case Results for Verify maven pom.xml is correct syntax

---
+++ src/test/pulse-test-executions/Verify maven pom.xml is correct syntax.spec.ts
@@ -0,0 +1,14 @@
+# Verify maven pom.xml is correct syntax
+
+Run the build command and make sure the build passes
+
+tags: maven,build
+
+## Run mvn test
+
+* Test cases should be executed
+
+## 
+
+* 
+
