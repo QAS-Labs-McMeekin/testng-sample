0e9c0a2cc939a4ec52a5866dab32f0757c56e884
kmcmeek91 <kylemcmeekin@qasymphony.com>
2017-03-02 11:34:00 -0500

    Test Case Results for Untitled Test Case McMeek

---
+++ gauge/tests/Untitled Test Case McMeek.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("Zstep - should - Zresult", function() {
+  assert.equal(true, "Zstep - should - Zresult");
+});
