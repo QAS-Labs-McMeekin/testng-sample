e0239b25713d376d7f889724ab204496af9bf65b
kmcmeek91 <kylemcmeekin@qasymphony.com>
2017-03-09 07:40:05 -0500

    Test Case Results for FizzBuzz

---
+++ gauge/tests/FizzBuzz.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("some step - should - some result", function() {
+  assert.equal(true, "some step - should - some result");
+});
